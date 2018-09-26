ab8cd0b9d228014e0c4d6d03282bdc9755773bd1
ws-morrison <workshop.morrison@gmail.com>
2018-09-27 01:41:18 +0300

    add dropwdown footer menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-$(function() {
+"use strict";
 
-	// Пользовательские функции
+// Dropdown menu
+$(document).ready(function() {
 
+  $(".footer-list").css({"display":"none"});
+
+  $(".opened-item").each(function() {
+    $(this).on("click", function() {
+      $(this)
+        .children(".open-list")
+        .slideToggle(300);
+    });
+  });
 });
