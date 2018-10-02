0720d927262200a562fb6180467d42f1406ee85c
ws-morrison <workshop.morrison@gmail.com>
2018-10-03 02:00:13 +0300

    add blocks, fxs

--- app/js/common.js
+++ app/js/common.js
@@ -2,23 +2,23 @@
 
 // Dropdown footer menu & Open/Close Info-services
 $(document).ready(function() {
-  $(".footer-list").css({ display: "none" });
-  $(".info-service-item").css({ display: "none" });
-  $(".opened-item").each(function() {
-    $(this).on("click", function() {
-      $(this)
-        .children(".open-list")
-        .slideToggle(300);
+    $(".footer-list").css({ display: "none" });
+    $(".info-service-item").css({ display: "none" });
+    $(".opened-item").each(function() {
+      $(this).on("click", function() {
+        $(this)
+          .children(".open-list")
+          .slideToggle(300);
 
-      // Change Title Color
-      $(this).toggleClass("info-services__title--active");
+        // Change Title Color
+        $(this).toggleClass("info-services__title--active");
+      });
     });
-  });
 
   // Dropdown header menu
   $(".burger").on("click", function() {
     $(".drop-menu").slideToggle(300);
-      $(".drop-menu").css('display', 'flex');
+    $(".drop-menu").css("display", "flex");
   });
 
   // Burger animation
@@ -27,4 +27,14 @@ $(document).ready(function() {
     $(".menu").toggleClass("opened-menu");
   });
 
+  $(window).resize(function(evt) {
+    var desktopBreakpoint = "1024px";
+    if ($(window) >= desktopBreakpoint) {
+      console.log("fwe");
+
+      $(".footer-list").css({ display: "block", marginRight: "10px" });
+      // $(".footer-item").removeClass("opened-item");
+      // $(".footer-list").removeClass("open-list");
+    }
+  });
 });
