7a7f89134e3d45183edd575488216c2270602a64
ws-morrison <workshop.morrison@gmail.com>
2018-10-03 15:52:28 +0300

    //

--- app/js/common.js
+++ app/js/common.js
@@ -1,40 +1,42 @@
 "use strict";
 
-// Dropdown footer menu & Open/Close Info-services
+// Dropdown footer menu
 $(document).ready(function() {
+  var onFooterMenu = function() {
     $(".footer-list").css({ display: "none" });
-    $(".info-service-item").css({ display: "none" });
     $(".opened-item").each(function() {
       $(this).on("click", function() {
         $(this)
           .children(".open-list")
-          .slideToggle(300);
-
-        // Change Title Color
-        $(this).toggleClass("info-services__title--active");
+          .slideToggle("fast");
       });
     });
+  };
+  onFooterMenu();
 
   // Dropdown header menu
-  $(".burger").on("click", function() {
-    $(".drop-menu").slideToggle(300);
-    $(".drop-menu").css("display", "flex");
-  });
+  var OnBurgerMenu = function() {
+    $(".burger").on("click", function() {
+      $(".drop-menu").slideToggle(300);
+      $(".drop-menu").css("display", "flex");
+    });
 
-  // Burger animation
-  $(".burger").click(function() {
-    $(this).toggleClass("open");
-    $(".menu").toggleClass("opened-menu");
-  });
+    // Burger animation
+    $(".burger").click(function() {
+      $(this).toggleClass("open");
+      $(".menu").toggleClass("opened-menu");
+    });
+  };
+  OnBurgerMenu();
 
-  $(window).resize(function(evt) {
-    var desktopBreakpoint = "1024px";
-    if ($(window) >= desktopBreakpoint) {
-      console.log("fwe");
+  var onInfoServices = function() {
+    $(".info-service-item").addClass("visually-hidden");
+    $(".info-services__wrapper").each(function() {
+      $(this).on("click", function() {
+        $(this).children('.info-service-item').toggleClass("visually-hidden");
+      });
+    });
+  };
+  onInfoServices();
 
-      $(".footer-list").css({ display: "block", marginRight: "10px" });
-      // $(".footer-item").removeClass("opened-item");
-      // $(".footer-list").removeClass("open-list");
-    }
-  });
 });
