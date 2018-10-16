f5de2f2f8652ca9320445472cb901531eed63beb
Alexey Makarov <alexeimakarow@gmail.com>
2018-10-16 16:15:27 +0200

    qweasd

--- docs/js/common.js
+++ docs/js/common.js
@@ -77,15 +77,19 @@ $(document).ready(function () {
     });
 
     //Datepicker https://fengyuanchen.github.io/datepicker/
-    // $(window).load(function () {
-    //     if ($(window).width() >= 1024) {
-    $('.jsDatePicker').datepicker({
-        language: 'ru-RU',
-        format: 'dd.mm.yyyy',
-        date: new Date(1980, 1, 1),
-        trigger: $('.jsCalendarBtn'),
+    $(window).load(function () {
+        if ($(window).width() >= 1024) {
+            $('.jsDatePicker').datepicker({
+                language: 'ru-RU',
+                format: 'dd.mm.yyyy',
+                date: new Date(1980, 1, 1),
+                trigger: $('.jsCalendarBtn'),
+            });
+        } else {
+            $('.patron-form__label--calendar-thumb').find('.jsDatePicker').each(function() {
+                $("<input type='date' class=\"jsDatePicker patron-form__input patron-form__input--date patron-form__input--date-mob\" placeholder=\"01.01.1980\" onfocus=\"this.placeholder=''\" onblur=\"this.placeholder = '01.01.1980'\" required/> ").insertBefore(this);
+            }).remove();
+        }
     });
-    // }
-    // });
 
 });
