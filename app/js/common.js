7b760195e4ecf8c7fe150494ffcb090a7fec44a7
Alexey Makarov <alexeimakarow@gmail.com>
2018-10-16 17:58:26 +0200

    asd

--- app/js/common.js
+++ app/js/common.js
@@ -77,46 +77,48 @@ $(document).ready(function () {
     });
 
     //Datepicker https://fengyuanchen.github.io/datepicker/
+
+    $('.jsDatePicker').datepicker({
+        language: 'ru-RU',
+        format: 'dd.mm.yyyy',
+        trigger: $('.jsCalendarBtn'),
+        template: '<div class="datepicker-container dp-container">\n' +
+        '  <div class="datepicker-panel dp-years" data-view="years picker">\n' +
+        '    <ul class="dp-years__nav">\n' +
+        '      <li data-view="years prev">&lsaquo;</li>\n' +
+        '      <li data-view="years current"></li>\n' +
+        '      <li data-view="years next">&rsaquo;</li>\n' +
+        '    </ul>\n' +
+        '    <ul data-view="years"></ul>\n' +
+        '  </div>\n' +
+        '  <div class="datepicker-panel dp-months" data-view="months picker">\n' +
+        '    <ul class="dp-months__nav">\n' +
+        '      <li data-view="year prev">&lsaquo;</li>\n' +
+        '      <li data-view="year current"></li>\n' +
+        '      <li data-view="year next">&rsaquo;</li>\n' +
+        '    </ul>\n' +
+        '    <ul data-view="months"></ul>\n' +
+        '  </div>\n' +
+        '  <div class="datepicker-panel dp-dates" data-view="days picker">\n' +
+        '    <ul class="dp-dates__nav">\n' +
+        '      <li data-view="month prev">&lsaquo;</li>\n' +
+        '      <li data-view="month current"></li>\n' +
+        '      <li data-view="month next">&rsaquo;</li>\n' +
+        '    </ul>\n' +
+        '    <ul class="dp-dates__week" data-view="week"></ul>\n' +
+        '    <ul class="dp-dates__days" data-view="days"></ul>\n' +
+        '  </div>\n' +
+        '</div>'
+    });
     // $(window).load(function () {
     //     if ($(window).width() >= 1024) {
-            $('.jsDatePicker').datepicker({
-                language: 'ru-RU',
-                format: 'dd.mm.yyyy',
-                trigger: $('.jsCalendarBtn'),
-                template: '<div class="datepicker-container dp-container">\n' +
-                '  <div class="datepicker-panel dp-years" data-view="years picker">\n' +
-                '    <ul class="dp-years__nav">\n' +
-                '      <li data-view="years prev">&lsaquo;</li>\n' +
-                '      <li data-view="years current"></li>\n' +
-                '      <li data-view="years next">&rsaquo;</li>\n' +
-                '    </ul>\n' +
-                '    <ul data-view="years"></ul>\n' +
-                '  </div>\n' +
-                '  <div class="datepicker-panel dp-months" data-view="months picker">\n' +
-                '    <ul class="dp-months__nav">\n' +
-                '      <li data-view="year prev">&lsaquo;</li>\n' +
-                '      <li data-view="year current"></li>\n' +
-                '      <li data-view="year next">&rsaquo;</li>\n' +
-                '    </ul>\n' +
-                '    <ul data-view="months"></ul>\n' +
-                '  </div>\n' +
-                '  <div class="datepicker-panel dp-dates" data-view="days picker">\n' +
-                '    <ul class="dp-dates__nav">\n' +
-                '      <li data-view="month prev">&lsaquo;</li>\n' +
-                '      <li data-view="month current"></li>\n' +
-                '      <li data-view="month next">&rsaquo;</li>\n' +
-                '    </ul>\n' +
-                '    <ul class="dp-dates__week" data-view="week"></ul>\n' +
-                '    <ul class="dp-dates__days" data-view="days"></ul>\n' +
-                '  </div>\n' +
-                '</div>'
-            });
-        // } else {
-        //
-        //     $('.patron-form__label--calendar-thumb').find('.jsDatePicker').each(function() {
-        //         $("<input type='date' class=\"jsDatePicker patron-form__input patron-form__input--date patron-form__input--date-mob\" placeholder=\"01.01.1980\" onfocus=\"this.placeholder=''\" onblur=\"this.placeholder = '01.01.1980'\" required/> ").insertBefore(this);
-        //     }).remove();
-        // }
+    // $('.jsDatePicker').datepicker();
+    // } else {
+    //
+    //     $('.patron-form__label--calendar-thumb').find('.jsDatePicker').each(function() {
+    //         $("<input type='date' class=\"jsDatePicker patron-form__input patron-form__input--date patron-form__input--date-mob\" placeholder=\"01.01.1980\" onfocus=\"this.placeholder=''\" onblur=\"this.placeholder = '01.01.1980'\" required/> ").insertBefore(this);
+    //     }).remove();
+    // }
     // });
 
 });
